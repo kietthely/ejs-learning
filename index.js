9817aa40becf752450a4b6449d3e596e19c77b2d
SammieDev <lythekiet333@gmail.com>
2024-10-17 19:44:02 +1030

    Render EJS file from javacsript file

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  const today = new Date();
+  const currentDay = today.getDay();
+  console.log(currentDay);
   res.render("index.ejs", {
     dayType: "a week day",
     advice: "It's time to learn coding!",
